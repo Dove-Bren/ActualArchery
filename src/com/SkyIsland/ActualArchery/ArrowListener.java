f72df7380eb91a12bcdccb6d7cce5e8bc8df460a
Skyler Manzanares <skymanzanares@hotmail.com>
2015-12-05 01:20:01 -0700

    Began to look into storage of meta

--- src/com/SkyIsland/ActualArchery/ArrowListener.java
+++ src/com/SkyIsland/ActualArchery/ArrowListener.java
@@ -1,6 +1,10 @@
 package com.SkyIsland.ActualArchery;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
 
 /**
  * Class dedicated to listening for arrow events and changing their actions to match the specified
@@ -9,6 +13,19 @@ import org.bukkit.event.Listener;
  */
 public class ArrowListener implements Listener {
 	
+	public ArrowListener() {
+		Bukkit.getPluginManager().registerEvents(this, ActualArcheryPlugin.plugin);
+	}
 	
+	@EventHandler
+	public void onArrowFire(ProjectileLaunchEvent e) {
+		if (e.isCancelled()) {
+			return;
+		}
+		
+		Projectile proj = e.getEntity();
+		
+		if (proj.getM)
+	}
 	
 }
